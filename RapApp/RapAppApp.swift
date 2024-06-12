68bea969a5a2811cabaa66408c78ff800f93d8c1
YutaKodama <suicaguy1124@gmail.com>
2024-06-12 19:31:40 +0900

    Initial Commit

---
+++ RapApp/RapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RapAppApp.swift
+//  RapApp
+//
+//  Created by yuta kodama on 2024/06/12.
+//
+
+import SwiftUI
+
+@main
+struct RapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
