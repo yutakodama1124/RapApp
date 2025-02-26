48ec2be6a317f685605f58186e63cd38084132e9
YutaKodama <suicaguy1124@gmail.com>
2025-02-26 18:42:42 +0900

    add: buttle me

---
+++ RapApp/Views/Battle/BattleMe.swift
@@ -0,0 +1,18 @@
+//
+//  BattleMe.swift
+//  RapApp
+//
+//  Created by yuta kodama on 2025/02/26.
+//
+
+import SwiftUI
+
+struct BattleMe: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BattleMe()
+}
